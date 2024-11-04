67f9ac6e56b1393abecd1412eb5d5a0433b0bb49
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-04 17:24:14 +0100

    Made some changes in settings

--- dev_school/settings.py
+++ dev_school/settings.py
@@ -38,10 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_app',
-    'admin',
-    'student',
-    'instructors',
+    'my_app', 
 ]
 
 MIDDLEWARE = [
