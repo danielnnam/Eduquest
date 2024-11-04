5f7c185b764c8269bd4bbdb2c707ec14efbda06c
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-04 23:44:20 +0100

    logic for login

--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'my_app/index.html')
+
+def login_view(request):
+    
+    return render(request, 'my_app/login.html')
