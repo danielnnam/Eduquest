0acdece5694ce8c65156647afa67eb3d426042cd
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-05 14:01:57 +0100

    created a view code for student registration

--- student/views.py
+++ student/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from student.forms import StudentRegistrationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+def register_student(request):
+    if request.method == 'POST':
+        form = StudentRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful! You can now log in.')
+            return redirect('login')  # Redirect to the login page
+    else:
+        form = StudentRegistrationForm()
+    
+    return render(request, 'students/register.html', {'form': form})
+
+
+@login_required
+def student_dashboard(request):
+    return render(request, 'students/dashboard.html')
