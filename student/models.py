98f8a56736c98219bd4f198a27f6108b68cda00f
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-05 14:03:08 +0100

    Student registration model code

--- student/models.py
+++ student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
