2036df677ef467563b3dbb3a386daf1a56514d85
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-05 14:09:01 +0100

    Modifyed the login_view code and the student_dashboard code

--- my_app/views.py
+++ my_app/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'my_app/index.html')
 
+
+@login_required
+def student_dashboard(request):
+    return render(request, 'student/dashboard.html')
+
+# Login View for Students and Teachers
 def login_view(request):
-    
-    return render(request, 'my_app/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if hasattr(user, 'student'):
+                    return redirect('student_dashboard')
+                elif hasattr(user, 'teacher'):
+                    return redirect('teacher_dashboard')
+                elif user.is_staff:
+                    return redirect('admin_dashboard')
+            else:
+                messages.error(request, 'Invalid credentials')
+    else:
+        form = LoginForm()
+    return render(request, 'my_app/login.html', {'form': form})
