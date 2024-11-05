7e38d0d2e37cf7d26919f0b9a80186149062914d
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-05 22:47:59 +0100

    Created a url code for course list

--- student/urls.py
+++ student/urls.py
@@ -6,5 +6,6 @@ urlpatterns =[
     # path('', include('my_app.urls')),
     path('register/', views.register_student, name='register_student'),
     path('dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('course_lists/', views.course_list, name='course_list'),
     path('logout/', views.user_logout, name='user_logout'),
 ]
