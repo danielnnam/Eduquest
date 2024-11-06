ad31bb4f65cccee98f8dd1048e2193bd082766b4
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-06 03:21:14 +0100

    Added a model code for instructor

--- instructors/models.py
+++ instructors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Instructor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
+    expertise = models.TextField()
+    phone = models.CharField(max_length=15, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
