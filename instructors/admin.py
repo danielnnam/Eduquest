cac8de60a23df0adf3c839a38963e3148fa3d72c
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-06 03:22:55 +0100

    Added instructor code to the admin panel

--- instructors/admin.py
+++ instructors/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Instructor
 
 # Register your models here.
+
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'department', 'is_approved')
+    actions = ['approve_instructors']
+
+    def approve_instructors(self, request, queryset):
+        for instructor in queryset:
+            instructor.is_approved = True
+            instructor.user.is_active = True  # Activate the user account
+            instructor.user.save()
+            instructor.save()
+        self.message_user(request, "Selected instructors have been approved.")
+
+admin.site.register(Instructor)
