dc8593c0da95d5708ee10b1722dac660248ae41d
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-08 14:03:37 +0100

    Added url for edit profile and change passwrod

--- student/urls.py
+++ student/urls.py
@@ -6,6 +6,8 @@ urlpatterns =[
     # path('', include('my_app.urls')),
     path('register/', views.register_student, name='register_student'),
     path('dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
+    path('change-password/', views.change_password, name='change_password'),
     path('course_lists/', views.course_list, name='course_list'),
     path('logout/', views.user_logout, name='user_logout'),
 ]
