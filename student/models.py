615fba100e9b34d426ae5f35c668f6f220d2051e
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-08 14:06:00 +0100

    Added a phone model into student and also created a user profile model

--- student/models.py
+++ student/models.py
@@ -8,6 +8,14 @@ class Student(models.Model):
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
     registration_date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
