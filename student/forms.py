ed2489b099c8ed17439b04cc12d3754f16d08989
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-08 14:07:52 +0100

    Modifyed the models import and also craeted a form PersonalInfoEditForm, PasswordChangeForm and UserProfileEditForm

--- student/forms.py
+++ student/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import Student, User
+from .models import Student, User, UserProfile
 
 class StudentRegistrationForm(forms.ModelForm):
     username = forms.CharField(max_length=150, required=True)
@@ -37,3 +37,52 @@ class StudentRegistrationForm(forms.ModelForm):
             if commit:
                 student.save()
         return student
+    
+    
+class PersonalInfoEditForm(forms.ModelForm):
+    class Meta:
+        model = User  # Assuming you're using Django's User model for authentication
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+              # Assuming you have a phone field in your User model or a related profile model
+        ]
+        widgets = {
+            
+           'first_name': forms.TextInput(attrs={
+               'class': 'form-control rounded-2 px-3',
+               'placeholder': 'Enter First name'
+           }),
+           'last_name': forms.TextInput(attrs={
+               'class': 'form-control rounded-2 px-3',
+               'placeholder': 'Last Name'
+           }),
+           'email': forms.EmailInput(attrs={
+               'class': 'form-control rounded-2 px-3',
+               'placeholder': 'Enter Last name'
+           }),
+           'username': forms.TextInput(attrs={
+               'class': 'form-control rounded-2 px-3',
+               'placeholder': 'Username'
+           })
+        }
+
+class PasswordChangeForm(forms.Form):
+    current_password = forms.CharField(widget=forms.PasswordInput, label="Current Password")
+    new_password = forms.CharField(widget=forms.PasswordInput, label="New Password")
+    confirm_new_password = forms.CharField(widget=forms.PasswordInput, label="Confirm New Password")
+
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['phone']  # Include 'phone' here
+        widgets = {
+            
+           'phone': forms.TextInput(attrs={
+               'class': 'form-control rounded-2 px-3',
+               'placeholder': 'Enter phone number'
+           })
+        }
