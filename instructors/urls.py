037fa930d333c66fb5b9e4da809782b5c453908f
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-12 15:35:30 +0100

    Created a url path for course list, course create, course update and course delete

--- instructors/urls.py
+++ instructors/urls.py
@@ -5,4 +5,8 @@ urlpatterns =[
     path('register/', views.register_instructor, name='register_instructor'),
     path('dashboard/', views.instructor_dashboard, name='instructor_dashboard'),
     path('not-approved/', views.not_approved, name='not_approved'),
+    path('courses/', views.instructor_course_list, name='instructor_course_list'),
+    path('courses/create/', views.instructor_course_create, name='instructor_course_create'),
+    path('courses/update/<int:course_id>/', views.instructor_course_update, name='instructor_course_update'),
+    path('courses/delete/<int:course_id>/', views.instructor_course_delete, name='instructor_course_delete'),
 ]
