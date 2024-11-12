d1c5333b0a85b4d14741b81548c0fed013ea1af1
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-12 01:57:45 +0100

    Added a course model and also modifyed the code

--- instructors/models.py
+++ instructors/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField 
+
 
 # Create your models here.
 
@@ -11,3 +14,43 @@ class Instructor(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+# creation of courses
+class Course(models.Model):
+    COURSE_CATEGORIES = [
+        ('Artificial Intelligence', 'Artificial Intelligence'),
+        ('Data Science', 'Data Science'),
+        ('Web Development', 'Web Development'),
+        ('Mobile App Development', 'Mobile App Development'),
+        ('Cloud Computing', 'Cloud Computing'),
+        ('Networking', 'Networking'),
+        ('Database Management', 'Database Management'),
+        ('Software Engineering', 'Software Engineering'),
+        ('IT Project Management', 'IT Project Management'),
+        ('Computer Science', 'Computer Science'),
+    ]
+
+    COURSE_LEVELS = [
+        ('Beginner', 'Beginner'),
+        ('Intermediate', 'Intermediate'),
+        ('Advanced', 'Advanced'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    duration = models.CharField(max_length=50, null=True)
+    credits = models.IntegerField(null=True)
+    category = models.CharField(max_length=255, choices=COURSE_CATEGORIES, null=True)
+    level = models.CharField(max_length=255, choices=COURSE_LEVELS, null=True)
+    syllabus = RichTextField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
