a46ab27e7717f8c3e45994f43314586337b3c914
cyril <145072652+Codes004@users.noreply.github.com>
2024-11-18 16:26:06 +0100

    Configured the file

--- instructors/urls.py
+++ instructors/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns =[
     path('register/', views.register_instructor, name='register_instructor'),
@@ -9,4 +11,11 @@ urlpatterns =[
     path('courses/create/', views.instructor_course_create, name='instructor_course_create'),
     path('courses/update/<int:course_id>/', views.instructor_course_update, name='instructor_course_update'),
     path('courses/delete/<int:course_id>/', views.instructor_course_delete, name='instructor_course_delete'),
+    path('course/details/<int:course_id>/', views.instructor_course_detail, name='instructor_course_detail'),
+
+
+    path('modules/add/<int:course_id>/', views.add_module, name='add_module'),  # Updated to include course_id
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
