6eaf4162f698917f69dd72c0d4900c877cee2a73
cyril <145072652+Codes004@users.noreply.github.com>
2024-12-03 17:41:03 +0100

    Created a url for the course details

--- student/urls.py
+++ student/urls.py
@@ -9,5 +9,6 @@ urlpatterns =[
     path('edit-profile/', views.edit_profile, name='edit_profile'),
     path('change-password/', views.change_password, name='change_password'),
     path('course_lists/', views.course_list, name='course_list'),
+    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
     path('logout/', views.user_logout, name='user_logout'),
 ]
