db68ff09748d23cb9d6fb4b83434f77d08e09a93
cyril <145072652+Codes004@users.noreply.github.com>
2025-09-22 16:28:48 +0100

    Models file

--- administration/models.py
+++ administration/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from instructors.models import Course
+from ckeditor.fields import RichTextField
+from django.utils.text import slugify
+
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = RichTextField()  # Rich text editor here
+    excerpt = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
