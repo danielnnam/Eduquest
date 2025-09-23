1ae30a9f019633f0d8269cce52e23c5117fd96c4
cyril <145072652+Codes004@users.noreply.github.com>
2025-09-23 14:28:19 +0100

    Modifyed the file

--- student/urls.py
+++ student/urls.py
@@ -13,5 +13,11 @@ urlpatterns =[
     path('add_to_cart/<int:course_id>/', views.add_to_cart, name='add_to_cart'),
     path('wishlist/', views.view_cart, name="view_cart"),
     path('cart/delete/<int:item_id>/', views.delete_cart_item, name='delete_cart_item'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('success/', views.success_page, name='success_page'),
+    path('my-courses/', views.my_courses, name='my_courses'),
+    path('my-course/<int:course_id>/content/', views.content_view, name='content_view'),
+    path('my-course/<int:course_id>/content/<int:content_id>/', views.content_view, name='content_view_with_content'),
+    path('transactions/', views.transaction_history, name='transaction_history'),
     path('logout/', views.user_logout, name='user_logout'),
 ]
