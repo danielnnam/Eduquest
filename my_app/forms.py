20179e86a9045db692191de60d2d1a09c1d3fc23
cyril <145072652+Codes004@users.noreply.github.com>
2025-09-23 15:49:47 +0100

    Modifyed the file

--- my_app/forms.py
+++ my_app/forms.py
@@ -4,3 +4,13 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class InstructorContactForm(forms.Form):
+    subject = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your message here...', 'rows': 5})
+    )
