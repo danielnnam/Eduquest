dce2bc105f69fa4ed18e06b66827f462e774a963
cyril <145072652+Codes004@users.noreply.github.com>
2025-09-23 15:52:16 +0100

    Admin file

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'author')
+    list_filter = ('created_at',)
