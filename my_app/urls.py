62494e326e297e2a06e68b027c7cf80154962b1e
cyril <145072652+Codes004@users.noreply.github.com>
2025-09-23 15:50:37 +0100

    Modifyed the file

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('student/', include('student.urls')),
@@ -9,6 +10,15 @@ urlpatterns = [
     path('administration/', include('administration.urls')),
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('instructor/pending/', views.instructor_pending_dashboard, name='instructor_pending_dashboard'),
+    path('courses/', views.courses_list, name='courses_list'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
+    path('blogs/<int:pk>/', views.blog_details, name='blog_details'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
 ]
 
 if settings.DEBUG:
