35075ace92739e93bd6ede2619165db383a7ae35
cyril <145072652+Codes004@users.noreply.github.com>
2025-09-25 14:52:03 +0100

    Modifyed the code

--- dev_school/settings.py
+++ dev_school/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = [
     '127.0.0.1',      # Localhost
     '192.168.173.194',    # Replace with your computer's local IP address
     '[::1]',          # IPv6 loopback
-    '*',              # Allow all hosts (not recommended for production)
 ]
 
 
@@ -86,6 +85,7 @@ CKEDITOR_CONFIGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -189,9 +189,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
